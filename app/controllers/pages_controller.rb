1c7aaf5c7a541d93fc6b0df41dd4e0adce106e1d
pradish mathew <pradish.mathew@gmail.com>
2011-12-14 11:15:03 -0600

    Statis Pages Commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home Page"
   end
 
   def contact
+  @title = "MyContacts"
+  end
+
+  def about
+  @title = "About rails"
   end
 
 end
